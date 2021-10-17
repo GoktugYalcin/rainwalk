1b2f2ea8bf60bc9f1282d2a53349bdab6e4eae89
GöktuğY <goktugyalcin06@gmail.com>
2021-10-17 13:24:07 +0300

    initial second

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, {useState, useEffect} from "react";
-import ReactPlayer from 'react-player/youtube'
+import ReactPlayer from 'react-player'
 import places from "./places";
 import './styles/App.css';
 import 'antd/dist/antd.css';
 import { Button, Drawer, Select, Divider, Slider, Space } from 'antd';
-import { MenuOutlined } from '@ant-design/icons';
+import { MenuOutlined, BackwardOutlined, ForwardOutlined } from '@ant-design/icons';
+import RadioBrowser from 'radio-browser'
 
 const { Option } = Select
-var player = {}
 
 const App = () => {
 
-  const [video, setVideo] = useState(places[0].link);
+  const [video, setVideo] = useState(places[0]);
   const [visible, setVisible] = useState(false);
   const [volume, setVolume] = useState(0.5);
-  const [player, setPlayer] = useState(null)
+  const [muted, setMuted] = useState(1);
 
+  const [stations, setStations] = useState([]);
+  const [stationIndex, setIndex] = useState(0);
+  const [station, setStation] = useState(null);
+
+  const [volumeRadio, setVolumeRadio] = useState(0);
+
+  useEffect(() => {
+    getRadioStations(places[0]).then((fetched)=>{
+      setStations(fetched);
+      setIndex(0)
+      setStation(fetched[0])
+      document.getElementById('radio').volume = 0
+    })
+  }, [])
 
   const showDrawer = () => {
     setVisible(true);
@@ -24,45 +38,22 @@ const App = () => {
     setVisible(false);
   };
 
-
-
-  const getPlaceNameByLink = (link) => {
-    for(var i in places){
-      if(places[i].link === link){
-        return places[i].place;
-      }
+  const getRadioStations = (video) => {
+    const filter = {
+      limit: 10,
+      by: "codec",
+      searchterm: "mp3"
     }
+    return RadioBrowser.getStations(filter)
   }
-
-  const _onReady = (event) => {
-    // player.ply = event.target;
-
-    // player.ply.playVideo()
-    // player.ply.seekTo(0)
-    console.log()
-  }
-
 
   const handleOnChange = (value) => {
+    console.log(value)
     console.log(value);
-    setVideo(value);
+    setVideo(places[value]);
+    getRadioStations(places[value]).then((fetch)=>{console.log(fetch);setIndex(0);setStations(fetch);setStation(stations[stationIndex])});
     setVisible(false);
   }
-
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      rel: 0,
-      autoplay: 1,
-      controls: 0,
-      disablekb: 1,
-      modestbranding: 1,
-      showinfo: 0,
-      autohide: 1,
-      loop: 1
-    }
-  };
 
   return(
     <>
@@ -71,18 +62,30 @@ const App = () => {
       <Button type="primary" style={{"background-color": "#C9A690", "border": "0"}} icon={<MenuOutlined />} size={'large'} onClick={showDrawer} />
       <Drawer title="☂RainWalk Menu" size={"large"} placement="right" onClose={onClose} visible={visible}>
         <Divider>Select the city</Divider>
-          <Select defaultValue={getPlaceNameByLink(video)} style={{ "margin-left": "30%", "margin-bottom": "10%", width: 180 }} size={ "large" } onChange={(e) => handleOnChange(e)}>
+          <Select defaultValue={video.place} style={{ "margin-left": "30%", "margin-bottom": "10%", width: 180 }} size={ "large" } onChange={(e) => handleOnChange(e)}>
             {places.map((value, index) => {
-              return(<Option key={index} value={value.link}> {value.place} </Option>)
+              return(<Option key={index} value={index}> {value.place} </Option>)
             })}
           </Select>
         <Divider>Change the volume</Divider>
         <Slider defaultValue={50} onChange={(value) => {setVolume(value/100)}} />
+        <Divider style={{"marginTop": "10%"}}>Radio Controls</Divider>
+          <div style={{"display": "flex", "justifyContent": "center", "width": "100%"}}>
+          <Space size={100} >
+            <BackwardOutlined style={{"fontSize": "40px"}} onClick={()=>{setIndex(stationIndex === 0 ? (0) : (stationIndex-1)); setStation(stations[stationIndex]); document.getElementById('radio').play();}} />
+            <label>{station ? (station.name) : ("Not Found Any Stations")}</label>
+            <ForwardOutlined style={{"fontSize": "40px"}} onClick={()=>{setIndex(stationIndex === stations.length ? (stations.length) : (stationIndex+1)); setStation(stations[stationIndex]); document.getElementById('radio').play();}} />
+          </Space>
+          </div>
+          <Slider defaultValue={0} onChange={(value) => {setVolumeRadio(value/100); document.getElementById('radio').volume=volumeRadio}} />
+          {/*  */}
       </Drawer>
       </div>
-      <ReactPlayer url={video} 
+      <ReactPlayer url={video.link} 
       volume={volume}
       playing={true}
+      onReady={()=>{setMuted(0)}}
+      className='react-player'
       config={{
         youtube: {
           playerVars: {
@@ -93,12 +96,14 @@ const App = () => {
             modestbranding: 1,
             showinfo: 0,
             autohide: 1,
-            loop: 1
+            loop: 1,
+            muted: muted
           }
         }
       }} 
       />
       </div>
+      <audio controls id={"radio"} src={station ? station.url_resolved : "http://cheetah.streemlion.com:1320/stream"} autoPlay> </audio>
     </>
   )
 }
--- src/places.js
+++ src/places.js
@@ -1,67 +1,83 @@
 const places = [
     {
         place: "Istanbul, TR",
-        link: "https://www.youtube.com/watch?v=G1BlRPQmxaE"
+        link: "https://www.youtube.com/watch?v=G1BlRPQmxaE",
+        country: "Turkey"
     },
     {
         place: "Boston, US",
-        link: "https://www.youtube.com/watch?v=JB0A8Me8EKk"
+        link: "https://www.youtube.com/watch?v=JB0A8Me8EKk",
+        country: "The United States of America"
     },
     {
         place: "London, UK",
-        link: "https://www.youtube.com/watch?v=N787WRdI35A"
+        link: "https://www.youtube.com/watch?v=N787WRdI35A",
+        country: "The United Kingdom Of Great Britain And Northern Ireland" //buna bak
     },
     {
         place: "Amsterdam, NE",
-        link: "https://www.youtube.com/watch?v=DR_owCQ-2PY"
+        link: "https://www.youtube.com/watch?v=DR_owCQ-2PY",
+        country: "ned" //buna da bak
     },
     {
         place: "Paris, FR",
-        link: "https://www.youtube.com/watch?v=z1rub7NjjPI"
+        link: "https://www.youtube.com/watch?v=z1rub7NjjPI",
+        country: "France" 
     },
     {
         place: "Madrid, SP",
-        link: "https://www.youtube.com/watch?v=TScr2bIJVv0"
+        link: "https://www.youtube.com/watch?v=TScr2bIJVv0",
+        country: "Spain" //buna da bak
     },
     {
         place: "Tokyo, JP",
-        link: "https://www.youtube.com/watch?v=JvoX86Epz1w"
+        link: "https://www.youtube.com/watch?v=JvoX86Epz1w",
+        country: "Japan"
     },
     {
         place: "Beijing, CN",
-        link: "https://www.youtube.com/watch?v=3_RezoBB8Zg"
+        link: "https://www.youtube.com/watch?v=3_RezoBB8Zg",
+        country: "China"
     },
     {
-        place: "Phnom Penh, CM",
-        link: "https://www.youtube.com/watch?v=q9h3Enzx3-U"
+        place: "Phnom Penh, KM",
+        link: "https://www.youtube.com/watch?v=q9h3Enzx3-U",
+        country: "Cambodia" // buna da bak
     },
     {
         place: "Liverpool, UK",
-        link: "https://www.youtube.com/watch?v=44hLEhmglL8"
+        link: "https://www.youtube.com/watch?v=44hLEhmglL8",
+        country: "The United Kingdom Of Great Britain And Northern Ireland"
     },
     {
         place: "New York, US",
-        link: "https://www.youtube.com/watch?v=BOa0zQBRs_M"
+        link: "https://www.youtube.com/watch?v=BOa0zQBRs_M",
+        country: "The United States of America"
     },
     {
         place: "Düsseldorf, GE",
-        link: "https://www.youtube.com/watch?v=IqlkrFb6cc8"
+        link: "https://www.youtube.com/watch?v=IqlkrFb6cc8",
+        country: "Germany" //buna da bak
     },
     {
         place: "Helsinki, FL",
-        link: "https://www.youtube.com/watch?v=YQ6KCESCXAc"
+        link: "https://www.youtube.com/watch?v=YQ6KCESCXAc",
+        country: "Finland" //buna da bak
     },
     {
         place: "Vancouver, CA",
-        link: "https://www.youtube.com/watch?v=4ra2QSsfzkM"
+        link: "https://www.youtube.com/watch?v=4ra2QSsfzkM",
+        country: "Canada"
     },
     {
         place: "Ilsan, KO",
-        link: "https://www.youtube.com/watch?v=PR516WiXDgQ"
+        link: "https://www.youtube.com/watch?v=PR516WiXDgQ",
+        country: "South Korea"
     },
     {
         place: "Melbourne, AU",
-        link: "https://www.youtube.com/watch?v=BKEN7f5qRBU"
+        link: "https://www.youtube.com/watch?v=BKEN7f5qRBU",
+        country: "Australia"
     }
 ]
 
